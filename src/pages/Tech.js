e9b738fd509de892896912469e0a4f59c1641c46
GitHubCatTest <129453284+GitHubCatTest@users.noreply.github.com>
2025-10-30 18:50:06 -0400

    Add src/pages/Tech.js

---
+++ src/pages/Tech.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Tech() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Tech</h2>
+      <p>This page will display technology news summaries.</p>
+    </div>
+  );
+}
+
+export default Tech;
