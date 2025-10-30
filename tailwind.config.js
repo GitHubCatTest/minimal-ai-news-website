ac1a1524350c662d16f948438d39507579d6e7a3
GitHubCatTest <129453284+GitHubCatTest@users.noreply.github.com>
2025-10-30 18:43:29 -0400

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#fdf6ee",
+        offwhite: "#fef8f2",
+        darktext: "#222222",
+      },
+    },
+  },
+  plugins: [],
+};
