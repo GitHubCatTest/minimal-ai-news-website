834425606bdfd0ac44e7f4ba8780f075c503f45a
GitHubCatTest <129453284+GitHubCatTest@users.noreply.github.com>
2025-10-30 18:50:37 -0400

    Add src/pages/Sports.js

---
+++ src/pages/Sports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sports() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Sports</h2>
+      <p>This page will display sports news summaries.</p>
+    </div>
+  );
+}
+
+export default Sports;
