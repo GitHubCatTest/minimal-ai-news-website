0d6fde869f4c654d2e86b12c76b50adb632e69b7
GitHubCatTest <129453284+GitHubCatTest@users.noreply.github.com>
2025-10-30 18:49:36 -0400

    Add src/pages/Politics.js

---
+++ src/pages/Politics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Politics() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Politics</h2>
+      <p>This page will display political news summaries.</p>
+    </div>
+  );
+}
+
+export default Politics;
